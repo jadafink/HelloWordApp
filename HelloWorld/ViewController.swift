7628ae311c88b70b64257cff1d5d86487e836b6a
Country.Girls <country.girls@109-b-0k2gfwm-m.manorisd.net>
2017-01-21 14:25:30 -0600

    Adding iOS app

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Background: UIImageView!
+    @IBOutlet weak var Banner: UIImageView!
+    @IBOutlet weak var WelcomeBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func WelcomePressed(_ sender: Any) {
+        Background.isHidden = false
+        Banner.isHidden=false
+        WelcomeBtn.isHidden = true
+    }
 
 }
 
